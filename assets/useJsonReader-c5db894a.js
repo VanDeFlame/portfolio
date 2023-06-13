35aae4394b6a01371e13dfe1667f08aead62cd7e
VanDeFlame <alexis-slash@outlook.com>
2023-06-13 17:54:10 -0300

    Updates

--- assets/useJsonReader-c5db894a.js
+++
@@ -1,1 +0,0 @@
-function t(){return o=>new Promise((e,r)=>{fetch(`portfolio/assets/${o}.json`).then(n=>n.json()).then(n=>{n?e(n):r(new Error(`Error reading ${o}. Empty data`))}).catch(n=>r(n))})}export{t as u};
